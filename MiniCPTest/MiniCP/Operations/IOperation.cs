57e87103e5207bb0053e2c32e5a78c7c11bb2b7b
Mohammad Saleh <mohammad.saleh.1988@gmail.com>
2018-01-24 16:27:14 +0100

    Add Rule and IOperation

---
+++ MiniCPTest/MiniCP/Operations/IOperation.cs
@@ -0,0 +1,6 @@
+﻿namespace MiniCP
+{
+    public interface IOperation
+    {
+    }
+}
---
+++ MiniCPTest/MiniCP/Rule.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniCP
+{
+    public class Rule
+    {
+        public object Operand1 { get; set; }
+        public object Operand2 { get; set; }
+        public IOperation Operation { get; set; }
+
+
+        public Rule Operand(object operand)
+        {
+            if (Operand1 == null)
+            {
+                this.Operand1 = operand;
+            }
+            else
+            {
+                this.Operand2 = operand;
+            }
+
+            return this;
+        }
+
+        public Rule Operator(IOperation operation)
+        {
+            this.Operation = operation;
+        }
+    }
+}
