394e5bd98c677908b20469e6973c604212397850
Saleh Mohammad <mohammad.saleh.1988@gmail.com>
2018-01-25 20:43:56 +0100

    Add new Tests Project

---
+++ MiniCPTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniCP;
+
+namespace MiniCPTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            var domainsList = new List<IDomain> {
+                new RangeDomain { LowBound = 1, HighBound = 3, Step = 1},
+                new RangeDomain { LowBound = 4, HighBound = 6 , Step = 1}
+            };
+
+            var x = new Variable { Domains = domainsList };
+
+            var rule = new SmallerThan { Operand1 = new Constant(5), Operand2 = x };
+
+            var context = new CPContext();
+            context.Add(x);
+            context.Add(rule);
+
+            //Act
+            var xAssignments = context.Evaluate();
+
+            //Assert
+            Assert.AreEqual(6, x.Value());            
+        }
+    }
+}
